dac04dec511dc6e8c32680015f2a086e85cfcc53
beomps <beomps@gmail.com>
2018-12-13 12:12:14 +0900

    Change login

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), 
+name='login'),
     url(r'', include('blog.urls')),
 ]
